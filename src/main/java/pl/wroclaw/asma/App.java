2f8770236f87952827c6839cd5767a6e8afac7fd
Maciej Anioł <maciej.aniol.programista@gmail.com>
2021-05-16 15:10:10 +0200

    feat: Weather app single city view added.

--- src/main/java/pl/wroclaw/asma/App.java
+++ src/main/java/pl/wroclaw/asma/App.java
@@ -2,8 +2,10 @@ package pl.wroclaw.asma;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -20,8 +22,17 @@ public class App extends Application {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("view/WeatherAppWindow.fxml"));
         Parent root = fxmlLoader.load();
         scene = new Scene(root);
+        scene.getStylesheets().add("WeatherApp.css");
+        stage.setTitle("Simple Weather App");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
+        //Positioning of application window in the right bottom corner
+        Rectangle2D bounds = Screen.getPrimary().getVisualBounds();
+        double x = bounds.getMinX() + (bounds.getWidth() - scene.getWidth()) * 0.99;
+        double y = bounds.getMinY() + (bounds.getHeight() - scene.getHeight()) * 0.92;
+        stage.setX(x);
+        stage.setY(y);
     }
 
     public static void main(String[] args) {
